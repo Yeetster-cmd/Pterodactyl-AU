fbd15e1334853b6795299cf144030d953420b142
Yeetster-cmd <64783847+Yeetster-cmd@users.noreply.github.com>
2022-02-08 20:37:55 +0200

    Update ptero-update.sh

--- ptero-update.sh
+++ ptero-update.sh
@@ -19,14 +19,14 @@ read -p $'Press 0 for Wings Update, Press 1 for Panel Update or press 2 to updat
 
 	if [ "$CHECK" = 1 ];
 	then
-		cd /var/www/pterodactyl && php artisan p:upgrade
+		cd /var/www/pterodactyl && php artisan p:upgrade -n
 		echo "Panel Update Completed"
 		echo "Quitting Script"
 	fi
 
 	if [ "$CHECK" = 2 ];
 	then
-		cd /var/www/pterodactyl && php artisan p:upgrade
+		cd /var/www/pterodactyl && php artisan p:upgrade -n
 		sleep 2
 		echo "Panel Update Completed"
 		curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
